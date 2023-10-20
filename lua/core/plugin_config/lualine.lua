8a554d93f0f47e79e7447c30f356913c9dcb58fd
Anirudh Prabhakaran <anirudhprabhakaran3@gmail.com>
2023-10-20 22:58:49 +0530

    Changed theme to onedark

--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -1,7 +1,7 @@
 require("lualine").setup {
   options = {
     icons_enabled = true,
-    theme = "gruvbox",
+    theme = "onedark",
     component_separators = { left = "|", right = "|" },
   },
   sections = {
